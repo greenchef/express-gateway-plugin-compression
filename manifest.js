e92a66278c02423a23ce2441f79369a7c8b45f84
David Clark <dclark@greenchef.com>
2019-08-20 12:32:17 -0600

    Get plugin working with gateway version 1.2.0.

--- manifest.js
+++ manifest.js
@@ -1,12 +1,50 @@
 const compression = require('compression');
 
 module.exports = {
-  version: '0.0.0',
-  policies: ['compression'],
+  version: '1.2.0',
   init: pluginContext => {
     pluginContext.registerPolicy({
       name: 'compression',
       policy: compression
     });
-  }
+  },
+  policies: ['compression'],
+  schema: {
+    $id: 'N/A',
+  },
 };
+
+// module.exports = {
+//   name: 'basic-to-jwt',
+//   schema: {
+//     $id: 'N/A',
+//     type: 'object',
+//     properties: {
+//       authUrl: { // Where to access the auth service for passing the basic credentials
+//         type: 'string',
+//       }
+//     }
+//   },
+//   policy: ({ authUrl }) => {
+//     return async (req, res, next) => {
+//       try {
+//         const authHeader = (req.headers || {}).authorization;
+//         if (authHeader && authHeader.startsWith('Basic ')) {
+//           const token = await request({
+//             uri: authUrl,
+//             headers: req.headers,
+//           });
+//           req.headers = {
+//             ...req.headers,
+//             authorization: `Bearer ${token}`,
+//           }
+//         }
+//       } catch (e) {
+//         console.error('Request to auth service failed. Error:', e.error)
+//         res.status(e.statusCode).send(e.message)
+//         return;
+//       }
+//       next();
+//     };
+//   }
+// };
