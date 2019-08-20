fd6e2e7a7b228259be2f7ae79d41ccfbf555ca2b
David Clark <dclark@greenchef.com>
2019-08-20 16:21:47 -0600

    Removed unneeded comment

--- manifest.js
+++ manifest.js
@@ -13,38 +13,3 @@ module.exports = {
     $id: 'N/A',
   },
 };
-
-// module.exports = {
-//   name: 'basic-to-jwt',
-//   schema: {
-//     $id: 'N/A',
-//     type: 'object',
-//     properties: {
-//       authUrl: { // Where to access the auth service for passing the basic credentials
-//         type: 'string',
-//       }
-//     }
-//   },
-//   policy: ({ authUrl }) => {
-//     return async (req, res, next) => {
-//       try {
-//         const authHeader = (req.headers || {}).authorization;
-//         if (authHeader && authHeader.startsWith('Basic ')) {
-//           const token = await request({
-//             uri: authUrl,
-//             headers: req.headers,
-//           });
-//           req.headers = {
-//             ...req.headers,
-//             authorization: `Bearer ${token}`,
-//           }
-//         }
-//       } catch (e) {
-//         console.error('Request to auth service failed. Error:', e.error)
-//         res.status(e.statusCode).send(e.message)
-//         return;
-//       }
-//       next();
-//     };
-//   }
-// };
